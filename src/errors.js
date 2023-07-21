9fd72ddc3c0cc2a017dd7114ae16376a569ef49a
Jeff Marion <jeff@marion.mx>
2023-07-20 17:20:33 -0700

    Tidy the interface and move versions to pre-1.0.0

--- src/errors.js
+++ src/errors.js
@@ -1,3 +1,5 @@
+'use strict';
+
 class CallbackRequiredError extends Error {
   constructor(message) {
     super(message);
--- src/utils/args.js
+++ src/utils/args.js
@@ -1,21 +1,25 @@
+'use strict';
 const pg = require('pg');
 
 function parseArgs(origArgs) {
   const result = {conn: null, pool: null, connOrPool: null, args: origArgs};
 
   const connOrPool = origArgs[0];
-  if (connOrPool instanceof pg.Client) {
-    result.conn = connOrPool;
-    result.connOrPool = connOrPool;
-    result.args = origArgs.slice(1);
+  if (connOrPool) {
+    const connOrPoolTypeName = connOrPool.constructor?.name;
+    if (connOrPool instanceof pg.Client || connOrPoolTypeName === 'Client' || typeof connOrPool._connected !== 'undefined') {
+      result.conn = connOrPool;
+      result.connOrPool = connOrPool;
+      result.args = origArgs.slice(1);
 
-    return result;
-  } else if (connOrPool instanceof pg.Pool) {
-    result.pool = connOrPool;
-    result.connOrPool = connOrPool;
-    result.args = origArgs.slice(1);
+      return result;
+    } else if (connOrPool instanceof pg.Pool || connOrPoolTypeName === 'BoundPool' || typeof connOrPool.Client !== 'undefined') {
+      result.pool = connOrPool;
+      result.connOrPool = connOrPool;
+      result.args = origArgs.slice(1);
 
-    return result;
+      return result;
+    }
   }
 
   return result;
